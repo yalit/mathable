640fd6f69d29af865de187f99f8a777e6ae30a65
Yannick <tech@yalit.be>
2025-08-25 21:46:23 +0200

    feat : fix home display

--- convex/queries/play.ts
+++ convex/queries/play.ts
@@ -1,11 +1,11 @@
-import {withSessionQuery} from "../middleware/sessions";
-import {v} from "convex/values";
-import {GamesQueryRepository} from "../repository/query/games.repository.ts";
-import {MovesQueryRepository} from "../repository/query/moves.repository.ts";
+import { withSessionQuery } from "../middleware/sessions";
+import { v } from "convex/values";
+import { GamesQueryRepository } from "../repository/query/games.repository.ts";
+import { MovesQueryRepository } from "../repository/query/moves.repository.ts";
 
 export const getCurrentTurnScore = withSessionQuery({
-    args: {gameId: v.id("games")},
-    handler: async (_, {gameId}): Promise<number> => {
+    args: { gameId: v.id("games") },
+    handler: async (_, { gameId }): Promise<number> => {
         const game = await GamesQueryRepository.instance.find(gameId)
         if (!game) {
             return 0;
--- src/Home.tsx
+++ src/Home.tsx
@@ -2,19 +2,19 @@ import { CreateGameCard } from "@components/home/createGameCard";
 import { SelectGame } from "@components/home/selectGame";
 
 function Home() {
-  return (
-    <div className="w-screen h-screen inset-0 grid grid-rows-3 gap-4">
-      <div className="h-full flex justify-center items-end font-bold text-4xl">
-        Mathable - game
-      </div>
-      <div className="row-span-2">
-        <div className="container border-amber-400 grid grid-cols-2 items-stretch gap-6">
-          <SelectGame />
-          <CreateGameCard />
+    return (
+        <div className="w-screen h-screen inset-0 grid grid-rows-3 gap-4">
+            <div className="h-full flex justify-center items-end font-bold text-4xl">
+                Mathable - game
+            </div>
+            <div className="row-span-2">
+                <div className="container border-amber-400 flex flex-col items-center gap-3">
+                    <SelectGame />
+                    <CreateGameCard />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Home;
