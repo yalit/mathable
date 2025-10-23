15358ca639bf9558fe01401630c28e178df17319
Yannick <tech@yalit.be>
2025-10-23 15:28:53 +0200

    refactor : remove console.logs

--- src/hooks/useJoinGame.tsx
+++ src/hooks/useJoinGame.tsx
@@ -3,7 +3,6 @@ import type { Player } from "@context/model/player";
 
 export const useJoinGame = () => {
   const joinGame = (game: Game, player: Player) => {
-    console.log("game, player", game, player);
     document.location = `/game/${game.token}/player/${player.token}`;
   };
 
