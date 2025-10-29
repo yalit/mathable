b194b96b184b086a7fed093d7bb271a443a3d827
Yannick <tech@yalit.be>
2025-10-29 09:46:58 +0100

    fix : fix board cell allowing drop if already tile in it

--- src/components/game/boardCell.tsx
+++ src/components/game/boardCell.tsx
@@ -1,10 +1,10 @@
 import { useMemo, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faDivide,
-  faMinus,
-  faPlus,
-  faXmark,
+    faDivide,
+    faMinus,
+    faPlus,
+    faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import type { Cell } from "@context/model/cell";
 import { classnames } from "@libraries/helpers/dom";
@@ -20,106 +20,109 @@ import { PickATileModal } from "./pickATileModal";
 import { NumberIcon } from "@components/includes/icon";
 
 export function BoardCell({ cell }: { cell: Cell }) {
-  const player = usePlayer();
-  const playTileToCell = useSessionMutation(
-    api.mutations.public.tile.playToCell,
-  );
-  const [displayPickModal, setDisplayPickModal] = useState<boolean>(false);
+    const player = usePlayer();
+    const playTileToCell = useSessionMutation(
+        api.mutations.public.tile.playToCell,
+    );
+    const [displayPickModal, setDisplayPickModal] = useState<boolean>(false);
 
-  const handleDropTile = (item: Tile) => {
-    playTileToCell({
-      tileId: item._id as Id<"tiles">,
-      cellId: cell._id as Id<"cells">,
-      playerId: player._id as Id<"players">,
-    });
-    //TODO : check if the game has still available tile
-    if (cell.type === "operator") {
-      setDisplayPickModal(true);
-    }
-  };
-
-  const [{ isOver, canDrop }, drop] = useDrop(() => {
-    return {
-      accept: DragItemTypes.TILE,
-      drop: handleDropTile,
-      canDrop: (item: Tile) => {
-        return player.current && cell.allowedValues.includes(item.value);
-      },
-      collect: (monitor) => ({
-        isOver: !!monitor.isOver(),
-        canDrop: !!monitor.canDrop(),
-      }),
+    const handleDropTile = (item: Tile) => {
+        playTileToCell({
+            tileId: item._id as Id<"tiles">,
+            cellId: cell._id as Id<"cells">,
+            playerId: player._id as Id<"players">,
+        });
+        //TODO : check if the game has still available tile
+        if (cell.type === "operator") {
+            setDisplayPickModal(true);
+        }
     };
-  }, [cell, cell.allowedValues]);
 
-  const cellContent = useMemo(() => {
-    if (cell.type === "empty") {
-      return null;
-    }
+    const [{ isOver, canDrop }, drop] = useDrop(() => {
+        return {
+            accept: DragItemTypes.TILE,
+            drop: handleDropTile,
+            canDrop: (item: Tile) => {
+                if (cell.tile) {
+                    return false
+                }
+                return player.current && cell.allowedValues.includes(item.value);
+            },
+            collect: (monitor) => ({
+                isOver: !!monitor.isOver(),
+                canDrop: !!monitor.canDrop(),
+            }),
+        };
+    }, [cell, cell.allowedValues]);
 
-    if (cell.type === "value") {
-      // return <FontAwesomeIcon icon={numberIcons[cell.value]} />;
-      return <NumberIcon icon={cell.value} />;
-    }
+    const cellContent = useMemo(() => {
+        if (cell.type === "empty") {
+            return null;
+        }
 
-    if (cell.type === "multiplier") {
-      switch (cell.multiplier) {
-        case 2:
-          return (
-            <>
-              <NumberIcon icon={2} />
-              <FontAwesomeIcon icon={faXmark} className="small" />
-            </>
-          );
-        case 3:
-          return (
-            <>
-              <NumberIcon icon={3} />
-              <FontAwesomeIcon icon={faXmark} className="small" />
-            </>
-          );
-      }
-    }
+        if (cell.type === "value") {
+            // return <FontAwesomeIcon icon={numberIcons[cell.value]} />;
+            return <NumberIcon icon={cell.value} />;
+        }
 
-    if (cell.type === "operator") {
-      switch (cell.operator) {
-        case "+":
-          return <FontAwesomeIcon icon={faPlus} />;
-        case "-":
-          return <FontAwesomeIcon icon={faMinus} />;
-        case "*":
-          return <FontAwesomeIcon icon={faXmark} />;
-        case "/":
-          return <FontAwesomeIcon icon={faDivide} />;
-      }
-    }
-  }, [cell.type]);
+        if (cell.type === "multiplier") {
+            switch (cell.multiplier) {
+                case 2:
+                    return (
+                        <>
+                            <NumberIcon icon={2} />
+                            <FontAwesomeIcon icon={faXmark} className="small" />
+                        </>
+                    );
+                case 3:
+                    return (
+                        <>
+                            <NumberIcon icon={3} />
+                            <FontAwesomeIcon icon={faXmark} className="small" />
+                        </>
+                    );
+            }
+        }
 
-  const cellClass = classnames(
-    "board__cell relative bg-sky-200 aspect-square p-1 flex justify-center items-center",
-    cell.type === "value" && "bg-sky-800! text-white",
-    cell.type === "multiplier" && cell.multiplier === 3 && "bg-white!",
-    cell.type === "multiplier" && cell.multiplier === 2 && "bg-sky-500!",
-    cell.type === "operator" && "bg-pink-300! text-white",
-  );
+        if (cell.type === "operator") {
+            switch (cell.operator) {
+                case "+":
+                    return <FontAwesomeIcon icon={faPlus} />;
+                case "-":
+                    return <FontAwesomeIcon icon={faMinus} />;
+                case "*":
+                    return <FontAwesomeIcon icon={faXmark} />;
+                case "/":
+                    return <FontAwesomeIcon icon={faDivide} />;
+            }
+        }
+    }, [cell.type]);
 
-  return (
-    <div className={cellClass} ref={drop}>
-      <div className="relative flex items-end gap-1">{cellContent}</div>
-      {cell.tile && (
-        <PlayTile tile={cell.tile} tileClass="absolute inset-0.5" />
-      )}
-      {isOver && (
-        <div
-          className={classnames(
-            "absolute inset-0 bg-red-300/50",
-            canDrop && "bg-green-300/50!",
-          )}
-        ></div>
-      )}
-      {displayPickModal && (
-        <PickATileModal closeModal={() => setDisplayPickModal(false)} />
-      )}
-    </div>
-  );
+    const cellClass = classnames(
+        "board__cell relative bg-sky-200 aspect-square p-1 flex justify-center items-center",
+        cell.type === "value" && "bg-sky-800! text-white",
+        cell.type === "multiplier" && cell.multiplier === 3 && "bg-white!",
+        cell.type === "multiplier" && cell.multiplier === 2 && "bg-sky-500!",
+        cell.type === "operator" && "bg-pink-300! text-white",
+    );
+
+    return (
+        <div className={cellClass} ref={drop}>
+            <div className="relative flex items-end gap-1">{cellContent}</div>
+            {cell.tile && (
+                <PlayTile tile={cell.tile} tileClass="absolute inset-0.5" />
+            )}
+            {isOver && (
+                <div
+                    className={classnames(
+                        "absolute inset-0 bg-red-300/50",
+                        canDrop && "bg-green-300/50!",
+                    )}
+                ></div>
+            )}
+            {displayPickModal && (
+                <PickATileModal closeModal={() => setDisplayPickModal(false)} />
+            )}
+        </div>
+    );
 }
