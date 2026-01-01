0d2a92d4caecd455c44cd623b9b1d10b7a0496ee
Yannick <tech@yalit.be>
2026-01-01 20:37:48 +0100

    ref : ref non-used imports

--- convex/controllers/cell/queries.ts
+++ convex/controllers/cell/queries.ts
@@ -2,7 +2,6 @@ import {appQuery} from "../../infrastructure/middleware/app.middleware.ts";
 import {v} from "convex/values";
 import type {GamesQueryRepositoryInterface} from "../../repository/query/games.repository.ts";
 import type {CellsQueryRepositoryInterface} from "../../repository/query/cells.repository.ts";
-import type {Cell} from "../../domain/models/Cell.ts";
 
 export const getForGame = appQuery({
     args: {gameId: v.union(v.id("games"), v.null())},
--- convex/controllers/tile/queries.ts
+++ convex/controllers/tile/queries.ts
@@ -2,7 +2,6 @@ import {appQuery} from "../../infrastructure/middleware/app.middleware.ts";
 import {v} from "convex/values";
 import type {GamesQueryRepositoryInterface} from "../../repository/query/games.repository.ts";
 import type {TilesQueryRepositoryInterface} from "../../repository/query/tiles.repository.ts";
-import type {Tile} from "../../domain/models/Tile.ts";
 
 export const getForGame = appQuery({
     args: {gameId: v.union(v.id("games"), v.null())},
